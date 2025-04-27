dbfe80092dc74e2c9873605f48b96a0aef1c4f51
JH20011123 <dlwngus25@gmail.com>
2025-04-28 01:30:23 +0900

    linked_list_queue_scheduling reupdate

--- OperatingSystem/project01-2020064257/linked_list_queue_scheduling/user/test.d
+++
@@ -1,2 +0,0 @@
-user/test.o: user/test.c /usr/riscv64-linux-gnu/include/stdc-predef.h \
- kernel/types.h kernel/stat.h user/user.h
--- OperatingSystem/project01-2020064257/linked_list_queue_scheduling/user/test1.d
+++
@@ -1,2 +0,0 @@
-user/test1.o: user/test1.c /usr/riscv64-linux-gnu/include/stdc-predef.h \
- kernel/types.h kernel/stat.h user/user.h
